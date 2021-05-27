08a71ca7157147a4a92e24c3f1e93005a05fe947
William Korando <wkorando@gmail.com>
2021-05-26 21:32:45 -0500

    Updating blog article for text block

--- text-block/src/main/java/TextBlockIV.java
+++ text-block/src/main/java/TextBlockIV.java
@@ -1,8 +1,6 @@
-
 public class TextBlockIV {
 
 	public static void main(String[] args) {
-
 		String aReallyLongLine = """
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit, \
 				sed do eiusmod tempor incididunt ut labore et dolore \
